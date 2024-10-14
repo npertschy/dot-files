e26d55586364a16a537389debbc2d3d7928f6850
Nikolas Pertschy <npertschy@inmediasp.de>
2024-10-14 15:06:38 +0200

    Initial commit

---
+++ install-tools-win.bat
@@ -0,0 +1,8 @@
+echo "Install WezTerm Terminal"
+winget install wez.wezterm
+
+echo "Install Starship prompt"
+winget install --id Starship.Starship
+
+echo "Install NeoVim"
+winget install Neovim.Neovim
