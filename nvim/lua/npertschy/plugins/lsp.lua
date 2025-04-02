df3e65a82162654ea12019348fc2364224fbefb9
Lina Unger <Lina@Mac.fritz.box>
2025-04-02 17:22:45 +0200

    Fixed vtsls

--- nvim/lua/npertschy/plugins/lsp.lua
+++ nvim/lua/npertschy/plugins/lsp.lua
@@ -205,19 +205,19 @@ return { -- LSP Configuration & Plugins
                 enableServerSideFuzzyMatch = true,
               },
             },
-          },
-          typescript = {
-            updateImportsOnFileMove = { enabled = 'always' },
-            suggest = {
-              completeFunctionCalls = true,
-            },
-            inlayHints = {
-              enumMemberValues = { enabled = true },
-              functionLikeReturnTypes = { enabled = true },
-              parameterNames = { enabled = 'literals' },
-              parameterTypes = { enabled = true },
-              propertyDeclarationTypes = { enabled = true },
-              variableTypes = { enabled = false },
+            typescript = {
+              updateImportsOnFileMove = { enabled = 'always' },
+              suggest = {
+                completeFunctionCalls = true,
+              },
+              inlayHints = {
+                enumMemberValues = { enabled = true },
+                functionLikeReturnTypes = { enabled = true },
+                parameterNames = { enabled = 'literals' },
+                parameterTypes = { enabled = true },
+                propertyDeclarationTypes = { enabled = true },
+                variableTypes = { enabled = false },
+              },
             },
             tsserver = {
               globalPlugins = {
