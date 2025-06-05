fbf72302472bc5af3689de0bcdec8ee39ffc18b6
Nikolas Pertschy <nikolas.pertschy@gmail.com>
2025-06-05 12:01:07 +0200

    Added neotest for java and vitest

---
+++ nvim/lua/npertschy/plugins/test-runner.lua
@@ -0,0 +1,103 @@
+return {
+  'nvim-neotest/neotest',
+  dependencies = {
+    'nvim-neotest/nvim-nio',
+    'nvim-lua/plenary.nvim',
+    'antoinemadec/FixCursorHold.nvim',
+    'nvim-treesitter/nvim-treesitter',
+    {
+      'rcasia/neotest-java',
+      ft = { 'java' },
+      dependencies = {
+        'mfussenegger/nvim-jdtls',
+        'mfussenegger/nvim-dap',
+        'rcarriga/nvim-dap-ui',
+        'theHamsta/nvim-dap-virtual-text',
+      },
+    },
+    {
+      'marilari88/neotest-vitest',
+      ft = { 'javascript', 'typescript', 'vue' },
+      keys = {
+        {
+          'leadertw',
+          function()
+            require('neotest').run.run { vitestCommand = 'vitest --watch' }
+          end,
+          desc = 'Run and watch test under cursor',
+        },
+        {
+          '<leader>tW',
+          function()
+            require('neotest').run.run { vim.fn.expand '%', vitestCommand = 'vitest --watch' }
+          end,
+          desc = 'Run and watch tests in current file',
+        },
+      },
+    },
+  },
+  opts = {
+    adapters = {
+      ['neotest-java'] = {},
+      ['neotest-vitest'] = {
+        filter_dir = function(name, rel_path, root)
+          return name ~= 'node_modules'
+        end,
+      },
+    },
+  },
+  keys = {
+    {
+      '<leader>tr',
+      function()
+        require('neotest').run.run()
+      end,
+      desc = 'Run test under cursor',
+    },
+    {
+      '<leader>tl',
+      function()
+        require('neotest').run.run_last()
+      end,
+      desc = 'Run last test',
+    },
+    {
+      '<leader>tf',
+      function()
+        require('neotest').run.run(vim.fn.expand '%')
+      end,
+      desc = 'Run tests in current file',
+    },
+    {
+      '<leader>td',
+      function()
+        require('neotest').run.run { strategy = 'dap' }
+      end,
+      desc = 'Run tests with DAP',
+    },
+    {
+      '<leader>ts',
+      function()
+        require('neotest').summary.toggle()
+      end,
+      desc = 'Toggle test summary',
+    },
+    {
+      '<leader>to',
+      function()
+        require('neotest').output.open {
+          enter = true,
+          auto_close = true,
+        }
+      end,
+      desc = 'Open test output',
+    },
+    {
+      '<leader>tp',
+      function()
+        require('neotest').output_panel.toggle()
+      end,
+      desc = 'Open test output panel',
+    },
+  },
+}
