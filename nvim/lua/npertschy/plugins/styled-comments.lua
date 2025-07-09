56e1799ddc7f632f3f41cf12aec4e443ba6c17ec
Nikolas Pertschy <nikolas.pertschy@dataciders.com>
2025-07-09 14:24:09 +0200

    Customize comment style pattern

--- nvim/lua/npertschy/plugins/styled-comments.lua
+++ nvim/lua/npertschy/plugins/styled-comments.lua
@@ -1,1 +1,10 @@
-return { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } }
+return {
+  'folke/todo-comments.nvim',
+  event = 'VimEnter',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  opts = {
+    highlight = {
+      pattern = [[.*<(KEYWORDS)\s*:?]],
+    },
+  },
+}
