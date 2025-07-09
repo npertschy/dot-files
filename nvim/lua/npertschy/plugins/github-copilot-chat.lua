0735eab1e56cad9cb7f6b0c8b7314435165d7006
Nikolas Pertschy <nikolas.pertschy@gmail.com>
2025-07-09 14:22:40 +0200

    Switched github copilot integration and added codecompanion

--- nvim/lua/npertschy/plugins/github-copilot-chat.lua
+++ nvim/lua/npertschy/plugins/github-copilot-chat.lua
@@ -1,65 +1,22 @@
 return {
-  {
-    'CopilotC-Nvim/CopilotChat.nvim',
-    lazy = true,
-    version = '*',
-    dependencies = {
-      { 'github/copilot.vim' },
-      { 'nvim-lua/plenary.nvim' },
+  'olimorris/codecompanion.nvim',
+  opts = {},
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-treesitter/nvim-treesitter',
+  },
+  keys = {
+    {
+      '<M-g>',
+      '<cmd>CodeCompanionChat Toggle<cr>',
+      desc = 'Copilot Chat',
+      mode = { 'n', 'v' },
     },
-    -- build = "make tiktoken", -- Only on MacOS or Linux
-    keys = {
-      {
-        '<M-g>',
-        '<cmd>CopilotChatToggle<cr>',
-        desc = 'Copilot Chat',
-      },
-    },
-    opts = {
-      mappings = {
-        complete = {
-          insert = '<Tab>',
-        },
-        close = {
-          normal = 'q',
-          insert = '<C-c>',
-        },
-        reset = {
-          normal = '<C-l>',
-          insert = '<C-l>',
-        },
-        submit_prompt = {
-          normal = '<CR>',
-          insert = '<C-s>',
-        },
-        toggle_sticky = {
-          detail = 'Makes line under cursor sticky or deletes sticky line.',
-          normal = 'gr',
-        },
-        accept_diff = {
-          normal = '<C-y>',
-          insert = '<C-y>',
-        },
-        jump_to_diff = {
-          normal = '<M-j>',
-        },
-        quickfix_diffs = {
-          normal = '<M-q>',
-        },
-        yank_diff = {
-          normal = '<M-y>',
-          register = '"',
-        },
-        show_diff = {
-          normal = '<M-d>',
-        },
-        show_system_prompt = {
-          normal = '<M-p>',
-        },
-        show_user_selection = {
-          normal = '<M-s>',
-        },
-      },
+    {
+      '<M-a>',
+      '<cmd>CodeCompanionActions<cr>',
+      desc = 'Copilot Chat Actions',
+      mode = { 'n', 'v' },
     },
   },
 }
