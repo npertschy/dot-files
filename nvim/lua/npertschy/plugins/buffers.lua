500fa2dbb0dfb9a7556bf44e7e6309c3d1db4853
Nikolas Pertschy <nikolas.pertschy@gmail.com>
2025-09-30 14:19:31 +0200

    Some qol adjustments
    - buffer tracking
    - better auto completion
    - defend overseer list to not be occupied

---
+++ nvim/lua/npertschy/plugins/buffers.lua
@@ -0,0 +1,15 @@
+return {
+  'bloznelis/buftrack.nvim',
+  config = function()
+    local buftrack = require 'buftrack'
+    buftrack.setup()
+
+    -- this will move the current buffer to the top of the buffer list.
+    vim.api.nvim_create_autocmd({ 'InsertEnter' }, {
+      callback = buftrack.track_buffer,
+    })
+
+    vim.keymap.set('n', '[b', buftrack.prev_buffer)
+    vim.keymap.set('n', ']b', buftrack.next_buffer)
+  end,
+}
