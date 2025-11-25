178f26f292e5f8daa819cbd76f18fa531f801c3c
Nikolas Pertschy <nikolas.pertschy@gmail.com>
2025-11-25 15:56:02 +0100

    Add useful keymap and restrict todo comment highlight

--- nvim/lua/npertschy/core/keymaps.lua
+++ nvim/lua/npertschy/core/keymaps.lua
@@ -38,6 +38,10 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- NOTE  move selected line up or down in visual mode
+vim.keymap.set('v', '<A-j>', ':m .+1<CR>==gv', { desc = 'Move selected line down' })
+vim.keymap.set('v', '<A-k>', ':m .-2<CR>==gv', { desc = 'Move selected line up' })
+
 -- NOTE: preserve selection when indenting
 vim.keymap.set('v', '<', '<gv')
 vim.keymap.set('v', '>', '>gv')
