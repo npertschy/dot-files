058f43616467465de91f18a79db4f43b4b75c567
Nikolas Pertschy <nikolas.pertschy@dataciders.com>
2026-06-02 11:48:44 +0200

    Adjustments to java config
    - fixed java home with brew
    - spring boot lsp gets initialized on first open buffer
    - more reliable workspace_dir resolution

--- nvim/lua/npertschy/plugins/java-language-server.lua
+++ nvim/lua/npertschy/plugins/java-language-server.lua
@@ -12,7 +12,18 @@ return {
       'mfussenegger/nvim-jdtls',
       'nvim-telescope/telescope.nvim',
     },
-    opts = {},
+    config = function()
+      local sb = require 'spring_boot'
+      -- When loaded lazily via `ft`, the FileType event has already fired for the
+      -- first buffer, so the autocmd registered by setup() misses it.
+      -- Manually start the Spring Boot LSP for the current buffer to compensate.
+      local launch = require 'spring_boot.launch'
+      local ls_path = sb.get_ls_from_mason()
+      if ls_path then
+        local boot_opts = vim.tbl_deep_extend('keep', { ls_path = ls_path }, require 'spring_boot.config')
+        launch.start(launch.update_ls_config(boot_opts))
+      end
+    end,
   },
   {
     'alessio-vivaldelli/java-creator-nvim',
