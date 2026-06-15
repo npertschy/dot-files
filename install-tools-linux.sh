a20779536c910f77baa9618f18bed37028c8add5
Nikolas Pertschy <nikolas.pertschy@dataciders.com>
2026-06-15 11:23:07 +0200

    Move away fro oh-my-zsh
    - installed tools eza, fzf, zoxide

--- install-tools-linux.sh
+++ install-tools-linux.sh
@@ -2,10 +2,6 @@
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
 brew install starship
 
 brew install fnm
@@ -14,18 +10,29 @@ fnm install 24
 
 brew install fd
 
+brew install ripgrep
+
 brew install neovim
 
 brew install git-delta
 
-brew install bat
+brew install eza
 
-brew install openjdk-21
+brew install zoxide
+# for zoxide auto-completion
+brew install fzf
+
+brew install openjdk@21
 
 brew install go
 
 brew install tree-sitter-cli
 
+brew install opencode
+
 brew install uv
 
 CC=gcc CXX=g++ uv tool install "vectorcode<1.0.0"
+
+brew install zsh-syntax-highlighting
+brew install zsh-autosuggestions
